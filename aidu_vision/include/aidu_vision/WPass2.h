49cbbc057b6601402632a29b404f0c6898592af6
Christiaan Brekelmans <christiaan@hotmail.fr>
2013-11-25 17:28:27 +0100

    beter?

--- aidu_vision/include/aidu_vision/WPass2.h
+++ aidu_vision/include/aidu_vision/WPass2.h
@@ -3,13 +3,18 @@
 
 #include <ros/ros.h>
 #include <aidu_core/node.h>
-#include <aidu_vision/Distance_sensors.h>
+#include <aidu_vision/DistanceSensors.h>
 
 
 namespace aidu {
   
-
-
+  class SensorHandler : public aidu::core::Node {
+  public:
+    
+  protected:
+    
+  };
+}
 
 
 
--- aidu_vision/src/WPass2.cpp
+++ aidu_vision/src/WPass2.cpp
@@ -1,2 +1,5 @@
 #include <aidu_vision/WPass2.h>
 
+int main(int argc, char* argv) {
+  return 0;
+}
