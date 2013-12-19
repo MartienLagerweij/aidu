5b9d7e2c5520d478db665d4d523e37d6fcb877c5
Rolf Jagerman <rjagerman@gmail.com>
2013-12-19 12:07:30 +0100

    Modified button detector with code cleanups and timing operations

--- aidu_elevator/src/button_detector.py
+++ aidu_elevator/src/button_detector.py
@@ -5,12 +5,27 @@ import os
 import scipy as sp
 import numpy as np
 import logging
+from timing import timing
+
+# Set logging to correct level
+logging.getLogger().setLevel(logging.INFO)
+
+# Get relative path to images, independent of current location
+data_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data/'))
+detection_directory = os.path.abspath(os.path.join(data_path, 'detection'))
+
+# Get all file names in the directory, and initialize images and processed images
+files = [file_name[2] for file_name in os.walk(detection_directory)][0]
+images = []
+processed_images = []
 
 
+@timing
 def get_image(file_name):
     return cv2.imread(os.path.join(detection_directory, file_name))
 
 
+@timing
 def process_image(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.medianBlur(image,5)
@@ -19,15 +34,19 @@ def process_image(image):
                 cv2.THRESH_BINARY,11,2)
     th3 = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
                 cv2.THRESH_BINARY,11,2)
-    return th2
+    return th3
 
-logging.getLogger().setLevel(logging.INFO)
-data_path = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data/'))
-detection_directory = os.path.abspath(os.path.join(data_path, 'detection'))
-files = [file_name[2] for file_name in os.walk(detection_directory)][0]
-images = [get_image(file_name) for file_name in files]
-processed_images = [process_image(image) for image in images]
-output_image = images[0]
+
+@timing
+def get_all_images():
+    global images, files
+    images = [get_image(file_name) for file_name in files]
+
+
+@timing
+def process_all_images():
+    global processed_images, images
+    processed_images = [process_image(image) for image in images]
 
 
 def switch_image(id):
@@ -47,11 +66,19 @@ def switch_image(id):
 
 
 def main():
-    size = len(images)
+    logging.info('Loading images...')
+    get_all_images()
 
+    logging.info('Processing all images...')
+    process_all_images()
+
+    logging.info('Constructing GUI...')
+    size = len(images)
     cv2.namedWindow('image')
     cv2.createTrackbar('Selector','image', 0, size, switch_image)
     switch_image(0)
+
+    logging.info('Done!')
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
