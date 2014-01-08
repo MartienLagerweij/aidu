8843b49faff3e09074dd9b00bf45f30bc3695880
Christiaan Brekelmans <christiaan@hotmail.fr>
2014-01-08 16:32:34 +0100

    implemented joint state publsiher

--- aidu_robotarm/include/aidu_robotarm/robot_arm.h
+++ aidu_robotarm/include/aidu_robotarm/robot_arm.h
@@ -4,6 +4,7 @@
 #include <ros/ros.h>
 #include <aidu_core/node.h>
 #include <aidu_robotarm/motor.h>
+#include <aidu_robotarm/test.h>
 
 namespace aidu {
     namespace mobile_robot_arm {
@@ -15,6 +16,7 @@ namespace aidu {
             ~Robot_arm();
             
             void spin(); ///< reads the position of the motors
+	    void testcallback(const aidu_robotarm::test::ConstPtr& msg);
 
         protected:
             
@@ -22,7 +24,11 @@ namespace aidu {
             aidu::mobile_robot_arm::Motor* rotationMotor;
             aidu::mobile_robot_arm::Motor* extensionMotor;
 	    
+	    double translation,rotation,extention;
+	    
 	    ros::Publisher joint_pub;
+	    ros::Subscriber test;
+	    
             
             void resetPos();
             double getLinearVelocity();
